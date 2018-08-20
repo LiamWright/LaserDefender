b7c4f1be2b07425d9af4501df2007f42ccef31c1
Leem <mail@liam-wright.co.uk>
2018-08-20 12:48:13 +0100

    Added Player Movement, assets, Scenes, Menus

---
+++ LaserDefender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 15.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+	}
+}

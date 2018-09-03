0f0bad1ac97843d5c644a71e0f711c9855ba477a
Leem <mail@liam-wright.co.uk>
2018-09-03 12:17:21 +0100

    Found the mistake. Damage in prefab had not been updated to 100.

--- LaserDefender/Assets/Entities/Enemy/EnemyBehaviour.cs
+++ LaserDefender/Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -6,6 +6,7 @@ public class EnemyBehaviour : MonoBehaviour {
     public float health = 150;
     void OnTriggerEnter2D(Collider2D collider)
     {
+       
         Projectile missile = collider.gameObject.GetComponent<Projectile>();
         if(missile)
         {
